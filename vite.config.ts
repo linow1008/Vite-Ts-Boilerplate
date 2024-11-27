8999e74cde164d4f6eb03ad2198e56e6db0272fe
이원일/(협력사) Engineer/로봇서비스개발Project(wonil.lee@lgepartner.com) <wonil.lee@lgepartner.com>
2024-11-27 16:35:13 +0900

    first

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import * as path from 'path';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    host: true,
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', // 백엔드 API 주소
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
